4cd38fe7af2e1d159412e109e52e6d57d1f2d9ea
root <root@LAPTOP-1HHUNG0V.localdomain>
2023-02-25 22:40:20 +0800

    Initial commit

---
+++ src/test/java/com/example/gb/GbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
